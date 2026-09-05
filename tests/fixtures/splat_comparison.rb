023c6e1d9ce028b8303fabe88c24cc12df2ed7f4
Reuben Sutton <reuben.sutton@gmail.com>
2026-09-05 11:52:25 -0400

    Add splat comparison fixture

---
+++ tests/fixtures/splat_comparison.rb
@@ -0,0 +1,35 @@
+# typed: true
+
+extend T::Sig
+
+sig { params(one: Integer, two: String).void }
+def fixed(one, two)
+end
+
+sig { params(values: Integer).void }
+def accepts_rest(*values)
+end
+
+sig { params(first: Integer, second: String).void }
+def fixed_keywords(first:, second:)
+end
+
+sig { returns(T::Array[T.untyped]) }
+def dynamic_positional
+  T::Array[T.untyped].new
+end
+
+sig { returns(T::Hash[Symbol, T.untyped]) }
+def dynamic_keywords
+  T::Hash[Symbol, T.untyped].new
+end
+
+# Rest parameters themselves are supported.
+accepts_rest(1, 2, 3)
+
+# A literal has a statically known shape and can be expanded.
+fixed(*[1, "two"])
+
+# These are the dynamic splats Sorbet documents as unsupported.
+fixed(*dynamic_positional)
+fixed_keywords(**dynamic_keywords)
