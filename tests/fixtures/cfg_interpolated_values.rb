324f9be0279c5f0538e4b4b20ddcf9cdb0d7702e
Reuben Sutton <reuben.sutton@gmail.com>
2026-09-08 21:39:54 -0400

    Correct interpolation conformance expectations

--- tests/fixtures/cfg_interpolated_values.rb
+++ tests/fixtures/cfg_interpolated_values.rb
@@ -1,7 +1,7 @@
 # typed: true
 
 value = 1
-T.reveal_type("value=#{value.to_s}") # revealed: String
-T.reveal_type(/value=#{value}/) # revealed: Regexp
-T.reveal_type(:"value_#{value}") # revealed: Symbol
-T.reveal_type(`echo #{value}`) # revealed: String
+T.reveal_type("value=#{value.to_s}") # note: String
+T.reveal_type(/value=#{value}/) # note: Regexp
+T.reveal_type(:"value_#{value}") # note: Symbol
+T.reveal_type(`echo #{value}`) # note: String
