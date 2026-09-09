ee4d6a67c36e19c7cc9078a14b19d4a05e823f7c
Reuben Sutton <reuben.sutton@gmail.com>
2026-09-09 01:07:44 -0400

    Cover legacy hash literal diagnostics

--- tests/fixtures/cfg_hash_literal_keys.rb
+++ tests/fixtures/cfg_hash_literal_keys.rb
@@ -12,7 +12,7 @@ class CfgHashLiteralKeys
   end
 
   #: -> String?
-  def formatter_name
+  def formatter_name # error: Expected method `formatter_name` to return
     options = {
       relative_file_paths: [], #: Array[String]
       formatter_name: nil, #: String?
@@ -21,12 +21,12 @@ class CfgHashLiteralKeys
   end
 
   #: -> Integer
-  def updated_value
+  def updated_value # error: Expected method `updated_value` to return
     options = {
       name: "value",
       other: false,
     }
-    options[:name] = 1
+    options[:name] = 1 # error: Expected `T.any(FalseClass, String)`, but found `Integer`
     options[:name]
   end
 end
@@ -37,7 +37,7 @@ T.reveal_type(CfgHashLiteralKeys.new.updated_value) # note: Integer
 
 class CfgIvarHashLiteralKeys
   #: -> Array[String]
-  def relative_file_paths
+  def relative_file_paths # error: Expected method `relative_file_paths` to return
     @options = {
       relative_file_paths: [], #: Array[String]
       ignore_nested_packages: false, #: bool
