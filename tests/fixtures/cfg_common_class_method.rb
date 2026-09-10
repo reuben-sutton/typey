fc8db90d490d91f7c8d655bac755bc0cf80c176a
Reuben Sutton <reuben.sutton@gmail.com>
2026-09-09 20:30:33 -0400

    Correct class reveal conformance expectations

--- tests/fixtures/cfg_common_class_method.rb
+++ tests/fixtures/cfg_common_class_method.rb
@@ -1,6 +1,5 @@
 # typed: true
 
-T.reveal_type(1.class) # note: Integer class
-T.reveal_type("text".class) # note: String class
-T.reveal_type([1].class) # note: Array class
-
+T.reveal_type(1.class) # note: Revealed type: `Class[Integer]`
+T.reveal_type("text".class) # note: Revealed type: `Class[String]`
+T.reveal_type([1].class) # note: Revealed type: `Class[Array]`
