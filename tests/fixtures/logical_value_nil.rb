e17f344d5635bfeb4626a8e0bdcdc3d8c51b8edb
Reuben Sutton <reuben.sutton@gmail.com>
2026-09-12 12:37:24 -0400

    Assert logical fixture reveal types

--- tests/fixtures/logical_value_nil.rb
+++ tests/fixtures/logical_value_nil.rb
@@ -5,8 +5,8 @@ def logical_value(value)
   value && value.to_s
 end
 
-T.reveal_type(logical_value(nil))
-T.reveal_type(logical_value("text"))
-T.reveal_type(nil && "text")
-T.reveal_type(nil)
-T.reveal_type(false && "text")
+T.reveal_type(logical_value(nil)) # note: T.nilable(String)
+T.reveal_type(logical_value("text")) # note: T.nilable(String)
+T.reveal_type(nil && "text") # note: NilClass
+T.reveal_type(nil) # note: NilClass
+T.reveal_type(false && "text") # note: FalseClass
